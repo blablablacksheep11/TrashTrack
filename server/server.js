fd361f6573c971744818028781ebfc7dc7451eeb
blablablacksheep11 <lamyongqin@gmail.com>
2025-07-05 11:00:26 +0800

    client side file path changed
    - img, client, src folder served in static
    - signin.html served as default

--- server/server.js
+++ server/server.js
@@ -6,6 +6,14 @@ import path from 'path';
 import { fileURLToPath, pathToFileURL } from 'url';
 import {socketSetup} from './services/socket.js';
 import { connectRedis } from './services/redis.js';
+import dotenv from 'dotenv';
+
+// __dirname replacement in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load .env from root directory
+dotenv.config({ path: path.join(__dirname, '../.env') });
 
 const app = express();
 const server = createServer(app); // The default express doesn't support WebSocket
@@ -14,14 +22,14 @@ const PORT = process.env.PORT || 3000;
 socketSetup(server); // Connect to WebSocket
 await connectRedis(); // Connect to Redis
 
-// __dirname replacement in ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 app.use(cors()); // Use cors to allow cross-origin requests
 app.use(express.json()); // Parse JSON request body
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request body, for POST CRUD operation
-app.use(express.static(path.join(__dirname, '../client'))); // Serve static files
+
+// Serve static files
+app.use(express.static(path.join(__dirname, '../client'))); 
+app.use(express.static(path.join(__dirname, '../src')));
+app.use(express.static(path.join(__dirname, '../img')));
 
 // Automatically load all route files
 const routesPath = path.join(__dirname, 'routes');
@@ -37,6 +45,11 @@ for (const file of routeFiles) {
   const routeModule = await import(pathToFileURL(fullPath).href);
   app.use(routePath, routeModule.default);
 }
+
+// Serve signin.html as the default page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/signin.html'));
+});
 
 // Serve the navbar
 app.get('/loadNavbar', (req, res) => {
